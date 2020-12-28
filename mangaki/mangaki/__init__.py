0aae0caf8ac2c0a0d16373cf54de7e8a02b4bbe3
Raito Bezarius <masterancpp@gmail.com>
2020-12-28 05:17:13 +0100

    celery: rename celery.py into workers.py to avoid all possible nameclashing with the Celery dependency module

--- mangaki/mangaki/__init__.py
+++ mangaki/mangaki/__init__.py
@@ -1,3 +1,3 @@
-# FIXME(Ryan): do we still need this import? from mangaki.celery import app as celery_app
+from mangaki.workers import app as celery_app
 
 default_app_config = 'mangaki.apps.MangakiConfig'
